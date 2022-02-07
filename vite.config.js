3577c86c3bd1eb1256f65dfb5f57c911770ffcc7
IBA4 <nabin7076@protonmail.com>
2022-02-07 19:15:36 +0545

    changes to help static & github pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
-
+    base: '/vite-wst/', //'/REPO_NAME/'to deploy static site for github-pages
     plugins: [
         svelte({
             configFile: 'svelte.config.js'
         }),
-        wasmPack(['./wasm-crate'])
+        wasmPack(['./wasm-crate'])// crate folder
     ]
 });
