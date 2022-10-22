d6696577ceef4f01b1221ab408e09af1dacb7fd3
IBA4 <nabin7076@protonmail.com>
2022-10-22 23:05:47 +0545

    upgrade pakage.json & update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
-    base: '/vite-wst/', //'/REPO_NAME/'to deploy static site for github-pages
+   // base: '/vite-wst/', //'/REPO_NAME/'to deploy static site for github-pages (uncomment only when github-pages is needed)
     plugins: [
         svelte({
             configFile: 'svelte.config.js'
